d4b3d0ff38aa99f5a8bd7fee488800d3eef2bbb7
jhoan34 <johanmonsalve125@gmail.com>
2025-01-24 16:45:02 -0500

    wdddddddddddddddddddddddtrrrrrrr

--- components/routes/routes.data.ts
+++ components/routes/routes.data.ts
@@ -1,25 +1,24 @@
 export const routes = [ 
+    
     {
-        id: 1,
-        name: "Graficos",
-        path: "/graficos"
+        id: 4,
+        name: "Home",
+        path: "/"
+
+    },
+    {
+        id: 5,
+        name: "gastos_ventas",
+        path: "/h"
     },
     {
         id: 2,
         name: "Historiales",
         path: "/historiales"
     },
-
     {
-        id: 4,
-        name: "Home",
-        path: "/"
-
-    }
-    , 
-    {
-        id: 5,
-        name: "gastos_ventas",
-        path: "/h"
+        id: 1,
+        name: "Graficos",
+        path: "/graficos"
     }
 ]
